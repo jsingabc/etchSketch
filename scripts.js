4911e0738e12eb5a3507571350024a118f478bfd
Jermain Singleton <jsingabc@gmail.com>
2023-09-01 16:05:18 -0400

    Adding comments to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
+//Main Container for divs, where i flex
 var container = document.createElement('div')
 container.setAttribute('id', 'container');
 document.body.appendChild(container);
 
+//Gave the looped divs a id
 var divGrid = document.createElement('div');
 divGrid.id = "divGrid";
 divGrid.dataset = "divGrid";
 
+//this loop gives me 256 solo divs
 for (let i = 0; i < 256; i++) {
      container.innerHTML += '<div id="divGrid" dataset="divGrid"></div>';
 }
 
+//atttempt to contact divs solo but failed
 let grids = document.getElementById("divGrid");
 
+//testing what i click on
 grids.addEventListener('click', (e) => {
     console.log(e);
 
     e.stopPropagation()
 })
 
-
+// function i pulled online to generate random # for colors
 function randomRBG(){
     let r = randomNumber(255);
     let b = randomNumber(255)
@@ -26,12 +31,13 @@ function randomRBG(){
     return [r,g,b];
 }
 
-
+// random number generator
 function randomNumber(max) {
     return Math.floor(Math.random()*(max + 1));
 }
 
-
+// function i pulled offline to take RBG function and conver 
+// into Hex that give me a usable color
 function randomHex() {
     let [r, b, g] = randomRBG();
 
